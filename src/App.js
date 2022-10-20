deceb2b753561377bfffb08ac49ed6f3ad822fec
dakhinkar <pranitdakhinkar145@gmail.com>
2022-10-20 11:42:53 +0530

    Add Routesto the components

--- src/App.js
+++ src/App.js
@@ -1,145 +1,35 @@
-import React, {startTransition, useState} from "react";
-import Card from "./Component/Card/Card";
-import Indicator from "./Component/Display/Indicator";
-import styles from './App.module.css';
+import React from 'react'
+import Home from './pages/home/Home';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import List from './pages/list/List';
+import Login from './pages/login/Login';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
 
 const App = () => {
-    // const [status, setStatus] = useState({
-    //     total: 0,
-    //     offline: 0,
-    //     alert: 0,
-    //     warning: 0,
-    //     normal : 0 
-    // // });
-    // const [offlineState, setOffline] = useState(0);
-    // const [alertState, setAlert] = useState(0);
-    // const [warningState, setWarning] = useState(0);
-    // const [normalState, setNormal] = useState(0);
-    // const [totalState, setTotal] = useState(0);
-
-    const data = [
-        {
-            id: 13,
-            deviceName: "ORI-CM-BUSH",
-            zone: "ZONE 000",
-            valueCal: {
-                value1: "Batt...",
-                value2: "24 C"
-            },
-            deviceStatus: {
-                status: "Offline",
-                value: "25 V"
-            },
-            timestampInfo: {
-                date: "06/06",
-                time: "17:49",
-                value: "24 C"
-            },
-            location: "GURGAON"
-
-        },
-        {
-            id: 14,
-            deviceName: "ORI-CM-BUSH",
-            zone: "ZONE 000",
-            valueCal: {
-                value1: "Batt...",
-                value2: "24 C"
-            },
-            deviceStatus: {
-                status: "Normal",
-                value: "25 V"
-            },
-            timestampInfo: {
-                date: "06/06",
-                time: "17:49",
-                value: "24 C"
-            },
-            location: "GURGAON"
-
-        },
-        {
-            id: 15,
-            deviceName: "ORI-CM-BUSH",
-            zone: "ZONE 000",
-            valueCal: {
-                value1: "8.60 V",
-                value2: "24 C"
-            },
-            deviceStatus: {
-                status: "Normal",
-                value: "25 V"
-            },
-            timestampInfo: {
-                date: "06/06",
-                time: "17:49",
-                value: "24 C"
-            },
-            location: "GURGAON"
-
-        },
-        {
-            id: 16,
-            deviceName: "ORI-CM-BUSH",
-            zone: "ZONE 000",
-            valueCal: {
-                value1: "Batt...",
-                value2: "24 C"
-            },
-            deviceStatus: {
-                status: "Offline",
-                value: "25 V"
-            },
-            timestampInfo: {
-                date: "06/06",
-                time: "17:49",
-                value: "24 C"
-            },
-            location: "GURGAON"
-
-        },
-        
-    ];
-
-    const initialState = () => {
-        let total = 0;
-        let offline = 0;
-        let alert = 0;
-        let normal = 0;
-        let warning = 0;
-        data.forEach((ele) => {
-            if (ele.deviceStatus.status === "Offline") {
-                offline++;
-            } else if (ele.deviceStatus.status === "Alert") {
-                alert++;
-            } else if (ele.deviceStatus.status === "Normal") {
-                normal++;
-            } else {
-                warning++;
-            }
-            total++;
-        })
-
-        return { total, alert, offline, normal, warning };
-
-    }
-
-    const [status, setStatus] = useState(initialState);
-    
-
     return (
-        <div>
-            <Indicator status={status}/>
-            <div className={styles.container} >
-                {
-                data && data.map((ele) => (<Card key={ele.id} data={ ele } />))
-                }
-                
-            </div>
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'>
+                        <Route index element={<Home />} />
+                        <Route path='login' element={<Login />} />
+                        <Route path='users'>
+                            <Route index element={<List />} />
+                            <Route path=':userId' element={<Single />} />
+                            <Route path='new' element={<New />} />
+                        </Route>
+                        <Route path='products'>
+                            <Route index element={<List />} />
+                            <Route path=':productId' element={<Single />} />
+                            <Route path='new' element={<New />} />
+                        </Route>
+                    </Route>
+                </Routes>
             
+            </BrowserRouter>
         </div>
-    );
-
+    )
 }
 
 export default App;
--- src/Component/Utilty/Bar.js
+++
@@ -1,9 +0,0 @@
-import styles from './Styles.module.css';
-const Bar = ({color}) => {
-    return (
-        <div className={styles.bar} style={{backgroundColor: color}}>
-            
-        </div>
-    );
-}
-export default Bar;
